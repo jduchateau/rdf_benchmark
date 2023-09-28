47685f96c697e6d777a7c941344cfa9da7158e45
Alexander Bigerl <alexander@bigerl.eu>
2023-09-28 11:52:01 +0200

    complete implementation, use mordern C++ style and fix compiler warnings.

--- b_rdf4cpp/measures.h
+++ b_rdf4cpp/measures.h
@@ -1,48 +1,47 @@
 #ifndef MEASURES
 #define MEASURES
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <time.h>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
+#include <ctime>
 
-unsigned long long get_nanosec() {
-    int status;
-    struct timespec timespec;
-    unsigned long long ret = 0;
-    status = clock_gettime(CLOCK_MONOTONIC, &timespec);
-    if (status != 0) {
-        perror("error in clock_gettime:");
-        exit(1);
-    }
-    ret += (unsigned long long) timespec.tv_sec * 1000000000;
-    ret += (unsigned long long) timespec.tv_nsec;
-    return ret;
+ulong get_nanosec() {
+  int status;
+  struct timespec timespec{};
+  unsigned long long ret = 0;
+  status = clock_gettime(CLOCK_MONOTONIC, &timespec);
+  if (status != 0) {
+    perror("error in clock_gettime:");
+    exit(1);
+  }
+  ret += static_cast<ulong>(timespec.tv_sec) * 1000000000UL + static_cast<ulong>(timespec.tv_nsec);
+  return ret;
 }
 
-int parse_line(char* line){
-    // This assumes that a digit will be found and the line ends in " Kb".
-    int i = strlen(line);
-    const char* p = line;
-    while (*p <'0' || *p > '9') p++;
-    line[i-3] = '\0';
-    i = atoi(p);
-    return i;
+long parse_line(char *line) {
+  // This assumes that a digit will be found and the line ends in " Kb".
+  auto i = static_cast<long>(strlen(line));
+  const char *p = line;
+  while (*p < '0' || *p > '9') p++;
+  line[i - 3] = '\0';
+  i = static_cast<long>(atoi(p));
+  return i;
 }
 
-int get_vmsize(){ //Note: this value is in KB!
-    FILE* file = fopen("/proc/self/status", "r");
-    int result = -1;
-    char line[128];
+long get_vmsize() { //Note: this value is in KB!
+  FILE *file = fopen("/proc/self/status", "r");
+  long result = -1;
+  char line[128];
 
-    while (fgets(line, 128, file) != NULL){
-        if (strncmp(line, "VmRSS:", 6) == 0){
-            result = parse_line(line);
-            break;
-        }
+  while (fgets(line, 128, file) != nullptr) {
+    if (strncmp(line, "VmRSS:", 6) == 0) {
+      result = parse_line(line);
+      break;
     }
-    fclose(file);
-    return result;
+  }
+  fclose(file);
+  return result;
 }
 
 #endif
--- b_rdf4cpp/run.cpp
+++ b_rdf4cpp/run.cpp
@@ -2,7 +2,6 @@
 #include <measures.h>
 
 #include <iostream>
-#include <regex>
 
 using namespace std;
 
@@ -10,63 +9,79 @@ using namespace std;
 #define DBO_PERSON "http://dbpedia.org/ontology/Person"
 #define DBR_VINCENT "http://dbpedia.org/resource/Vincent_Descombes_Sevoie"
 
-int query([[maybe_unused]] int argc, char *argv[]) {
-  // TODO: argc, program and task are both unused ATM
-  [[maybe_unused]] char *program=argv[0];
-  [[maybe_unused]] char *task=argv[1];
-  
-  unsigned long long t0, t1;
-  double time_load, time_first = 0, time_rest;
-  long m1, mem_graph;
-  // queries.
-  t0 = get_nanosec();
-  std::string s = argv[2];
-  const char* filename = s.c_str();
-  fprintf(stderr, "%s: Parsing %s\n", program, filename);
-  long m0 = get_vmsize();
+enum class benchmark_queries : u_int8_t {
+    query_1 = 1,
+    query_2 = 2
+};
 
+void query(const std::string &filename, benchmark_queries query) {
+  // queries.
+  std::cerr << "rdf4cpp: Parsing " << filename << std::endl;
+  auto const mem_load_start = get_vmsize();
+  auto const time_load_start = get_nanosec();
   rdf4cpp::rdf::Graph g;
   {
     using namespace rdf4cpp::rdf;
-    for (const auto& e : rdf4cpp::rdf::parser::RDFFileParser{filename}) {
+    for (const auto &e: rdf4cpp::rdf::parser::RDFFileParser{filename}) {
       if (e.has_value()) {
         auto const &quad = e.value();
         g.add(Statement{quad.subject(), quad.predicate(), quad.object()});
-      }
-      else
-        std::cout << e.error();
+      } else
+        std::cerr << e.error();
     }
   }
-  t1 = get_nanosec();
-  time_load = (t1-t0)/1e9;
-  
-  m1 = get_vmsize();
-  //fprintf(stderr, "Memory before and after loading %i %i\n", m0, m1);
-  mem_graph = m1-m0; // difference may be 0 with 10k triples
-  
-  t0 = get_nanosec();
-   
-  int count = 0;
+  auto const time_load_done = get_nanosec();
+  auto const duration_load = static_cast<double>(time_load_done - time_load_start) / 1e9;
+  auto const mem_delta_load = get_vmsize() - mem_load_start; // difference may be 0 with 10k triples
+  decltype(get_nanosec())
+          time_query_first_result;
+
+  ulong count = 0;
   // Enumerate all triples matching a pattern
   {
     using namespace rdf4cpp::rdf;
-    query::TriplePattern triple_pattern{query::Variable("x"), IRI{RDF_TYPE}, IRI{DBO_PERSON}};
-    query::SolutionSequence solutions = g.match(triple_pattern);
+    using namespace rdf4cpp::rdf::query;
+    auto triple_pattern = (query == benchmark_queries::query_1) ?
+                          TriplePattern{Variable("x"), IRI{RDF_TYPE}, IRI{DBO_PERSON}} :
+                          TriplePattern{IRI(DBR_VINCENT), Variable("y"), Variable("z")};
+    SolutionSequence solutions = g.match(triple_pattern);
     std::cout << "g size " << g.size() << std::endl;
-    for ([[maybe_unused]] const auto &solution: solutions) {
+    for ([[maybe_unused]]const auto &solution: solutions) {
       if (count == 0) {
-        t1 = get_nanosec();
-        time_first = (t1 - t0) / 1e9;
-        t0 = get_nanosec();
+        time_query_first_result = get_nanosec();
       }
       count++;
     }
   }
-  t1 = get_nanosec();
-  time_rest = (t1-t0)/1e9;
-  fprintf(stderr, "%s: got %d matching statements\n", program, count);
-  printf("%f,%ld,%f,%f\n", time_load, mem_graph, time_first, time_rest);
-  return 0;
+  auto const time_query_done = get_nanosec();
+  auto const duration_query_first = static_cast<double>(time_query_first_result - time_load_done) / 1e9;
+  auto const duration_query_rest = static_cast<double>(time_query_done - time_query_first_result) / 1e9;
+  std::cerr << "rdf4cpp: got " << count << "matching statements" << std::endl;
+  printf("%f,%ld,%f,%f\n", duration_load, mem_delta_load, duration_query_first, duration_query_rest);
+}
+
+void parse(const std::string &filename) {
+  std::cerr << "rdf4cpp: Parsing " << filename << std::endl;
+
+  auto const mem_start = get_vmsize();
+  auto const time_start = get_nanosec();
+  ulong count = 0;
+  {
+    using namespace rdf4cpp::rdf;
+    parser::ParsingFlags flags = parser::ParsingFlag::Turtle;
+    for (const auto &e: parser::RDFFileParser{filename, flags}) {
+      if (e.has_value()) {
+        count++;
+      } else
+        std::cerr << e.error();
+    }
+  }
+  auto const time_passed = static_cast<double>(get_nanosec() - time_start) / 1e9;
+  auto const mem_delta = get_vmsize() - mem_start; // difference may be 0 with 10k triples
+
+  std::cout << time_passed << std::endl;
+  std::cerr << "rdf4cpp: parsed " << count << " triples" << std::endl;
+  std::cerr << "rdf4cpp: mem delta " << mem_delta << std::endl;
 }
 
 int main(int argc, char *argv[]) {
@@ -74,17 +89,16 @@ int main(int argc, char *argv[]) {
     fprintf(stderr, "Usage: %s <task> <filename> [options...]\n", argv[0]);
     return 1;
   }
-  const char *task = argv[1];
-  /*if (strcmp(task, "parse") == 0) {
-      return main_parse(argc, argv);
-  } else */
-  if (strcmp(task, "query") == 0) {
-    return query(argc, argv);
-  } /* else if (strcmp(task, "query2") == 0) {
-       return main_query(argc, argv);
-   } */
-  else {
-    fprintf(stderr, "Unknown task %s\n", task);
-    return 1;
+  std::string const task{argv[1]};
+  if (task == "parse") {
+    parse(argv[2]);
+  } else if (task == "query") {
+    query(argv[2], benchmark_queries::query_1);
+  } else if (task == "query2") {
+    query(argv[2], benchmark_queries::query_2);
+  } else {
+    std::cerr << "Unknown task " << task << std::endl;
+    return EXIT_FAILURE;
   }
+  return EXIT_SUCCESS;
 }
